05a52c5872e3447ed71985d17e18c8942a30193d
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2020-09-29 18:01:48 +0200

    Hecho

--- src/ule/iso1/singleton/VATtoApply.java
+++ src/ule/iso1/singleton/VATtoApply.java
@@ -6,16 +6,21 @@ public class VATtoApply {
 
    
     private double vatType;
-   
+    private static VATtoApply instance;
+    
     /**
      * MODIFICAR PARA CONSEGUIR QUE SEA SINGLETON
      */
 
-    public VATtoApply(double vat) {
+    private VATtoApply(double vat) {
     	vatType=vat;
     }
   
-   
+   public static VATtoApply getInstance(double vat) {
+	   if(instance == null)
+		   instance = new VATtoApply(vat);
+	   return instance;
+   }
 
 	public double getVatType() {
 		return vatType;
